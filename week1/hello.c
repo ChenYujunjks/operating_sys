8d8f7bf62d4da9c7ebe2ae2073e5c0a55e4581ff
bruceY <aaaeeettt124@126.com>
2024-09-11 15:32:34 +0800

    new

---
+++ week1/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "hello.h"
+
+void printidkdkdk_hello() {
+    printf("Hello, World!\n");
+}
---
+++ week1/main.c
@@ -0,0 +1,6 @@
+#include "hello.h"
+
+int main() {
+    printidkdkdk_hello();
+    return 0;
+}
