59fcbabef37f692185a80bb4f2f5103ffa595d74
bruce <aaaeeettt124@126.com>
2024-09-16 17:40:06 +0800

    ejfao

--- hw/hw1/q4.c
+++ hw/hw1/q4.c
@@ -1,12 +1,12 @@
-#include <stdlib.h>    // for exit
-#include <sys/types.h> // for pid_t (optional, used in some systems for fork return type)
-#include <unistd.h>    // for fork
+#include <stdlib.h> // for exit
+#include <stdio.h>
+#include <unistd.h> // for fork
 
 int main(int argc, char *argv[])
 {
     if (fork() != 10)
     {
-        printf(333);
+        printf("a\n");
     }
 
     return 0;
